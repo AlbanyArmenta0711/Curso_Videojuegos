18421a2c73a1e076d8cf3c232d66aafb6e63fca1
AlbanyArmenta0711 <albany.armenta@uabc.edu.mx>
2025-06-16 16:31:34 -0800

    Completed

--- asteroids/scenes/game_over/game_over.gd
+++ asteroids/scenes/game_over/game_over.gd
@@ -1,10 +1,13 @@
 extends CanvasLayer
 var can_press_space
 
+@onready var anim_player = $AnimationPlayer
+@onready var continue_label = $MarginContainer/VBoxContainer/ContinueLabel
+
 func _ready():
 	visible = false 
 	SignalManager.on_player_hit.connect(on_player_hit)
-	can_press_space = false
+	continue_label.visible = false
 	
 func _process(delta):
 	if(Input.is_action_just_pressed("shoot") and can_press_space):
@@ -16,4 +19,7 @@ func _process(delta):
 func on_player_hit():
 	get_tree().paused = true 
 	visible = true 
+	anim_player.play("game_over_text_anim")
+	continue_label.visible = true
+	anim_player.queue("modulate_continue")
 	can_press_space = true
